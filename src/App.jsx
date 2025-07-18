b5e0376e66a580a1159bbaae82533cee53e0ea68
Rajeev kumar <rajeevroy70701@gmail.com>
2025-07-18 20:17:20 +0530

    reade-file

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
       <div className="h-full max-w-4xl mx-auto flex flex-col p-3">
         {/* Fixed Header */}
         <header className="text-center py-4">
-          <a href="https://github.com/Vishesh-Pandey/chat-ai" 
+          <a href="https://github.com/rajeevroy21/Chat-ai" 
              target="_blank" 
              rel="noopener noreferrer"
              className="block">
